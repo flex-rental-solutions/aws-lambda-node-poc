99b5cec1e081d1ec13b1cf92ee7bce98ee1a9e91
Roger Diller <roger@flexrentalsolutions.com>
2020-11-04 11:14:42 -0500

    syncing with Lambda side

--- index.js
+++ index.js
@@ -4,24 +4,30 @@ exports.handler = async (event) => {
 
     let apiInfoResp = null;
 
-    let requestData = JSON.parse(event.body);
+    let requestData = event;
     let flexJWT = requestData.flexJWT;
     let modelId = requestData.modelId
 
-    axios.defaults.headers.common['X-Auth-Token'] = flexJWT // for all requests
+    let config = {
+        headers: {
+            "X-Auth-Token": flexJWT,
+        }
+    }
 
     try {
-        apiInfoResp = await axios.get('https://flex5dev.flexrentalsolutions.com/f5/api/inventory-model/' + modelId);
+        apiInfoResp = await axios.get('https://flex5dev.flexrentalsolutions.com/f5/api/inventory-model/' + modelId, config);
         //console.log(apiInfoResp.data);
     } catch (error) {
         console.error(error);
     }
 
     try {
-        let slackData = {"text" : JSON.stringify(apiInfoResp.data)};
         //post to #lambda_posts channel
-        let slackResp = await axios.post("https://hooks.slack.com/services/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", slackData);
-        //console.log(slackResp.data);
+        let slackData = {
+            "text": "AWS Lamba Data Post\n" + JSON.stringify(apiInfoResp.data)
+        }
+
+        let slackResp = await axios.post("https://hooks.slack.com/services/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", slackData);
     } catch (error) {
         console.error(error);
     }
