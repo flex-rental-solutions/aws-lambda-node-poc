68032311abef340cf479f1c2319c182b2746201d
Roger Diller <roger@flexrentalsolutions.com>
2020-11-04 09:44:16 -0500

    try axios request

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
+const axios = require('axios');
+
 exports.handler = async (event) => {
+
+    let apiInfoResp = null;
+
+    try {
+        apiInfoResp = await axios.get('https://flex5dev.flexrentalsolutions.com/f5/api/info');
+    } catch (error) {
+        console.error(error);
+    }
+
     const response = {
         statusCode: 200,
-        body: JSON.parse(event.body).msg
+        body: apiInfoResp
     };
     return response;
 };
