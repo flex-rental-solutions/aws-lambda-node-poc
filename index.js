66987b17ad42e550fbca91a103fbad1db0908f39
Roger Diller <roger@flexrentalsolutions.com>
2020-11-04 09:33:54 -0500

    JSON.parse

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 exports.handler = async (event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda and Github and Roger!! Whoa!!! ::: Event Msg:' + event.body.msg),
+        body: JSON.parse(event.body).msg
     };
     return response;
 };
