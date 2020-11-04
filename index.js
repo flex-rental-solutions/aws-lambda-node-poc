54f9d6bbff007b4764af86c5445be7cec2d05f35
Roger Diller <roger@flexrentalsolutions.com>
2020-11-04 10:00:40 -0500

    extract model

--- index.js
+++ index.js
@@ -4,8 +4,14 @@ exports.handler = async (event) => {
 
     let apiInfoResp = null;
 
+    let requestData = JSON.parse(event.body);
+    let flexJWT = requestData.flexJWT;
+    let modelId = requestData.modelId
+
+    axios.defaults.headers.common['X-Auth-Token'] = flexJWT // for all requests
+
     try {
-        apiInfoResp = await axios.get('https://flex5dev.flexrentalsolutions.com/f5/api/info');
+        apiInfoResp = await axios.get('https://flex5dev.flexrentalsolutions.com/f5/api/inventory-model/' + modelId);
         //console.log(apiInfoResp.data);
     } catch (error) {
         console.error(error);
